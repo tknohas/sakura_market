7cebcca1de45d357295f6936e4e0d9a3ee544a50
{tknohas} <0xv17851726325v@gmail.com>
2024-09-17 23:36:17 +0900

    ユーザーの新規登録およびログインができるように

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module SakuraMarket
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.default_locale = :ja
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  # Defines the root path route ("/")
-  # root "posts#index"
+  mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  root to: "home#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords'
+  }
 end

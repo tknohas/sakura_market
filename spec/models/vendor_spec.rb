1105cc81a4becd4586c0aacd5804250e3236ebdb
{tknohas} <0xv17851726325v@gmail.com>
2024-09-28 12:41:44 +0900

    spec実装

---
+++ spec/models/vendor_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe Vendor, type: :model do
+  let(:vendor) { create(:vendor) }
+  subject { described_class.new(name: 'アリスファーム', email: 'alicefarm@example.com', password: 'Abcd1234') }
+
+  describe 'バリデーション' do
+    it 'バリデーションが有効' do
+      expect(subject).to be_valid
+    end
+
+    it '業者名が不正' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'メールアドレスが不正' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'パスワードが不正(入力なし)' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'パスワードが不正(文字数不足)' do
+      subject.password = '123456'
+      expect(subject).to_not be_valid
+    end
+
+    it 'パスワードが不正(条件を満たしていない)' do
+      subject.password = 'abcd1234'
+      expect(subject).to_not be_valid
+    end
+  end
+end

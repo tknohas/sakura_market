902db264026564a5ad0562e708d88349de2bfe32
{tknohas} <0xv17851726325v@gmail.com>
2024-09-28 12:41:38 +0900

    管理者は業者アカウントの管理ができるように

---
+++ app/controllers/admin/vendors_controller.rb
@@ -0,0 +1,33 @@
+class Admin::VendorsController < Admin::ApplicationController
+  before_action :set_vendor, only: %i[edit update destroy]
+
+  def index
+    @vendors = Vendor.order(:id)
+  end
+
+  def edit
+  end
+
+  def update
+    if @vendor.update(vendor_params)
+      redirect_to admin_vendors_path, notice: '変更しました。'
+    else
+      render :edit, alert: '変更に失敗しました。', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @vendor.destroy!
+    redirect_to admin_vendors_path, notice: '削除に成功しました。'
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :email)
+  end
+
+  def set_vendor
+    @vendor = Vendor.find(params[:id])
+  end
+end

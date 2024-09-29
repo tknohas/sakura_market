7ba93fa0cce84d2066d0e77ae37f326cdd4ade10
{tknohas} <0xv17851726325v@gmail.com>
2024-09-29 15:11:43 +0900

    spec実装

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -8,7 +8,7 @@ module LoginModule
       click_button 'ログイン'
     end
   end
-  
+
   def user_login(user)
     visit new_user_session_path
     fill_in 'user_email', with: user.email
@@ -16,5 +16,15 @@ module LoginModule
     within '.form-actions' do
       click_button 'ログイン'
     end
+  end
+
+  def vendor_login(vendor)
+    visit new_vendor_session_path
+    fill_in 'vendor_email', with: vendor.email
+    fill_in 'vendor_password', with: vendor.password
+    within '.form-actions' do
+      click_button 'ログイン'
+    end
+    visit vendor_products_path
   end
 end

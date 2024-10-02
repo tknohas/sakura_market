bf84ffa89b4b65775bcf398bb616bd2e4bccabc6
{tknohas} <0xv17851726325v@gmail.com>
2024-10-02 10:53:41 +0900

    バリデーション修正

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,6 @@ class Stock < ApplicationRecord
   belongs_to :product
   belongs_to :vendor
 
-  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :product_id, uniqueness: { scope: :vendor_id }
 end

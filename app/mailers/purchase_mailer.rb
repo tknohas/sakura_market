bdcfcc96c80eaa487fd9a7fbb39abbdf8a46575a
{tknohas} <0xv17851726325v@gmail.com>
2024-10-02 16:25:05 +0900

    商品が購入されたら業者にメールが届くように

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -3,4 +3,11 @@ class PurchaseMailer < ApplicationMailer
     @purchase = purchase
     mail(to: purchase.user.email, subject: '購入が完了しました。')
   end
+
+  def notify_sold(purchase)
+    vendor = purchase.purchase_items.includes(:vendor).first.vendor
+    @vendor = vendor
+    @purchase = purchase
+    mail(to: vendor.email, subject: '商品が購入されました。')
+  end
 end
